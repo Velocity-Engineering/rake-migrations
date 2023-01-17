d00eec737809c3094d06b59c257e38efe08be263
Mani_Bhushan <mani_bhushan@ip-192-168-3-164.ec2.internal>
2023-01-17 11:43:28 +0530

    added fields

--- lib/rails/generators/rake_migrations/templates/migration_rails_after_v4.rb
+++ lib/rails/generators/rake_migrations/templates/migration_rails_after_v4.rb
@@ -3,7 +3,7 @@ class CreateRakeMigrationsTable < ActiveRecord::Migration<%= @rails_major_minor_
     create_table :rake_migrations do |t|
       t.string :version
       t.string :namespace
-      t.string :task
+      t.string :task_name
       t.string :created_by
       t.datetime :created_at
       t.integer :duration

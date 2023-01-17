799137ae10c8631f57914f891d86f2a0e32bbc32
Mani_Bhushan <mani_bhushan@ip-192-168-3-164.ec2.internal>
2023-01-17 11:20:10 +0530

    added fields

--- lib/rails/generators/rake_migrations/templates/migration_rails_after_v4.rb
+++ lib/rails/generators/rake_migrations/templates/migration_rails_after_v4.rb
@@ -2,12 +2,11 @@ class CreateRakeMigrationsTable < ActiveRecord::Migration<%= @rails_major_minor_
   def self.up
     create_table :rake_migrations do |t|
       t.string :version
-      t.string :uuid, null: false, unique: true
-      t.string :payee_id
-      t.string :bank_partner_id
-      t.string :account_number
-      t.string :account_type
-      t.string :ifsc_code
+      t.string :namespace
+      t.string :task
+      t.string :created_by
+      t.datetime :created_at
+      t.integer :duration
     end
   end
 

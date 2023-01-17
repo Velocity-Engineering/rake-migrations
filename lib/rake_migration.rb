9faaba3dc5ebf3bc8a9bb3aa352cba8d3aec8a14
Mani_Bhushan <mani_bhushan@ip-192-168-3-164.ec2.internal>
2023-01-17 12:29:58 +0530

    timezone

--- lib/rake_migration.rb
+++ lib/rake_migration.rb
@@ -5,14 +5,14 @@ class RakeMigration < ::ActiveRecord::Base
 
   def self.findcreate_by_version(rake_id, namespace, task_name, created_by, duration)
     find_or_create_by(
-      version: rake_id, namespace: namespace, task_name: task_name, created_by: created_by, duration: duration, created_at: Time.now.utc.getlocal("+05:30"))
+      version: rake_id, namespace: namespace, task_name: task_name, created_by: created_by, duration: duration, created_at: Time.now.utc.in_time_zone('Asia/Kolkata'))
   end
 
   def self.mark_complete(file, namespace, task_name, created_by, duration)
     rake_id = version_from_path(file)
     if methods.include?(:find_or_create_by)
       find_or_create_by(
-        version: rake_id, namespace: namespace, task_name: task_name, created_by: created_by, duration: duration, created_at: Time.now.utc.getlocal("+05:30"))
+        version: rake_id, namespace: namespace, task_name: task_name, created_by: created_by, duration: duration, created_at: Time.now.utc.in_time_zone('Asia/Kolkata'))
     else
       findcreate_by_version(rake_id, namespace, task_name, created_by, duration)
     end
